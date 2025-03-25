00a1546dda2b5e15ff685194f87a85fd440b8a6a
Md. Sabik Alam Rahat <sabikrahat72428@gmail.com>
2025-03-25 19:29:35 +0600

    Add branding images and update launch backgrounds for improved UI consistency

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,8 +7,8 @@ plugins {
 
 android {
     namespace = "com.algoramming.app"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    compileSdk = 35
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -24,8 +24,8 @@ android {
         applicationId = "com.algoramming.app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        minSdk = 22
+        targetSdk = 35
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
